6c6353f58ac50bf5cdea83fbd26b944a24b7c9fd
Alex (Oleksii) Haponiuk <git@alexh.dev>
2026-05-07 15:41:34 +0100

    Add Log enum (for later optimization)

--- src/stacks.rs
+++ src/stacks.rs
@@ -15,11 +15,19 @@ pub enum Operation {
     Rrr,
 }
 
+// Ignore operations will be ignored when optimization is ON
+// Later, optimizer will go over the Logs, and switch off useless ops
+#[derive(Debug)]
+pub enum Log {
+    Execute(Operation),
+    Ignore(Operation),
+}
+
 #[derive(Debug)]
 pub struct StackPair {
     a: VecDeque<usize>,
     b: VecDeque<usize>,
-    logs: Vec<Operation>,
+    logs: Vec<Log>,
 }
 
 impl StackPair {
@@ -32,54 +40,79 @@ impl StackPair {
         }
     }
 
+    // Idea here is that if operation wasn't successful,
+    // it means it would do nothing.
+    // So, in optimization mode, we just gonna ignore it.
+    // In case of double ops, we do OR,
+    // because if at least 1 op was needed,
+    // then we keep it the way it is, since its cost is 1 op anyway
     pub fn execute(&mut self, op: Operation) {
-        match op {
+        let success = match op {
             Operation::Sa => StackPair::swap(&mut self.a),
             Operation::Sb => StackPair::swap(&mut self.b),
             Operation::Ss => {
-                StackPair::swap(&mut self.a);
-                StackPair::swap(&mut self.b);
+                let a = StackPair::swap(&mut self.a);
+                let b = StackPair::swap(&mut self.b);
+                a || b
             }
             Operation::Pa => StackPair::push(&mut self.a, &mut self.b),
             Operation::Pb => StackPair::push(&mut self.b, &mut self.a),
             Operation::Ra => StackPair::rotate(&mut self.a),
             Operation::Rb => StackPair::rotate(&mut self.b),
             Operation::Rr => {
-                StackPair::rotate(&mut self.a);
-                StackPair::rotate(&mut self.b);
+                let a = StackPair::rotate(&mut self.a);
+                let b = StackPair::rotate(&mut self.b);
+                a || b
             }
             Operation::Rra => StackPair::rev_rotate(&mut self.a),
             Operation::Rrb => StackPair::rev_rotate(&mut self.b),
             Operation::Rrr => {
-                StackPair::rev_rotate(&mut self.a);
-                StackPair::rev_rotate(&mut self.b);
+                let a = StackPair::rev_rotate(&mut self.a);
+                let b = StackPair::rev_rotate(&mut self.b);
+                a || b
             }
-
-        }
-        self.logs.push(op);
+        };
+        let entry = if success {
+            Log::Execute(op)
+        } else {
+            Log::Ignore(op)
+        };
+        self.logs.push(entry);
     }
 
-    fn swap(stack: &mut VecDeque<usize>) {
+    fn swap(stack: &mut VecDeque<usize>) -> bool {
         if stack.len() >= 2 {
             stack.swap(0, 1);
+            true
+        } else {
+            false
         }
     }
 
-    fn push(dst: &mut VecDeque<usize>, src: &mut VecDeque<usize>) {
+    fn push(dst: &mut VecDeque<usize>, src: &mut VecDeque<usize>) -> bool {
         if let Some(val) = src.pop_front() {
             dst.push_front(val);
+            true
+        } else {
+            false
         }
     }
 
-    fn rotate(stack: &mut VecDeque<usize>) {
+    fn rotate(stack: &mut VecDeque<usize>) -> bool {
         if let Some(val) = stack.pop_front() {
             stack.push_back(val);
+            true
+        } else {
+            false
         }
     }
 
-    fn rev_rotate(stack: &mut VecDeque<usize>) {
+    fn rev_rotate(stack: &mut VecDeque<usize>) -> bool {
         if let Some(val) = stack.pop_back() {
             stack.push_front(val);
+            true
+        } else {
+            false
         }
     }
 }
